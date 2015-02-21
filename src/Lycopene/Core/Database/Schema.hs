3db2fe2c47ae3b587c80a75f3fd255dcc776d332
utky <ilyaletre@gmail.com>
2015-02-21 20:21:59 +0900

    primary key should be not null

--- src/Lycopene/Core/Database/Schema.hs
+++ src/Lycopene/Core/Database/Schema.hs
@@ -8,13 +8,13 @@ import           Lycopene.Core.TH
 schema :: String
 schema = [str|
 CREATE TABLE project (
-  project_id INTEGER PRIMARY KEY AUTOINCREMENT,
+  project_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
   name TEXT NOT NULL UNIQUE,
   description TEXT
 );
 
 CREATE TABLE sprint (
-  sprint_id INTEGER PRIMARY KEY AUTOINCREMENT,
+  sprint_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
   name TEXT NOT NULL,
   description TEXT,
   project_id INTEGER,
@@ -24,19 +24,25 @@ CREATE TABLE sprint (
 );
 
 CREATE TABLE issue (
-  issue_id INTEGER PRIMARY KEY AUTOINCREMENT,
+  issue_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
   title TEXT NOT NULL,
   description TEXT,
   sprint_id INTEGER,
   status INTEGER NOT NULL,
   FOREIGN KEY(sprint_id) REFERENCES sprint(sprint_id)
+  FOREIGN KEY(status) REFERENCES issue_status(status_id)
 );
 
 CREATE TABLE record (
-  record_id INTEGER PRIMARY KEY AUTOINCREMENT,
+  record_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
   issue_id INTEGER,
   start_on INTEGER,
   end_on INTEGER,
   FOREIGN KEY(issue_id) REFERENCES issue(issue_id)
+);
+
+CREATE TABLE issue_status (
+  status_id INTEGER PRIMARY KEY NOT NULL,
+  status_name INTEGER NOT NULL
 );
 |]
