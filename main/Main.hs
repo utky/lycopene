fecd12c61fd422deb005db4a731ffb20bac37a67
utky <ilyaletre@gmail.com>
2015-04-16 08:16:31 +0900

    more consice

--- main/Main.hs
+++ main/Main.hs
@@ -9,7 +9,9 @@ import System.Exit
 import Pipes as P
 
 main :: IO ()
-main = parseLycoCommand >>= (P.runEffect . Ps.runProcess . Ps.processCommand) >>= exitWith . mapExit where
+main = parseLycoCommand >>= runCommand >>= exitWith . mapExit where
+  runCommand :: LycoCommand -> IO Result
+  runCommand = P.runEffect . Ps.runProcess . Ps.processCommand
   parseLycoCommand = O.execParser lycoParser
   mapExit Ps.Success = ExitSuccess
   mapExit _       = ExitFailure 1
