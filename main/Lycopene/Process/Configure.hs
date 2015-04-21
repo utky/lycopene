1b620b945afc9e63165f3bb0d096ee00b3aac8e0
utky <ilyaletre@gmail.com>
2015-04-22 08:20:46 +0900

    impld basic IO in configure

--- main/Lycopene/Process/Configure.hs
+++ main/Lycopene/Process/Configure.hs
@@ -1,15 +1,23 @@
 module Lycopene.Process.Configure where
 
+import qualified Data.Text as T
 import           System.FilePath ((</>), dropFileName)
-import           System.Directory (createDirectoryIfMissing)
-import           Lycopene.Process.Core (ProcessM, out, MonadIO)
+import           System.Directory (createDirectoryIfMissing, getHomeDirectory, doesFileExist)
+import           Lycopene.Process.Core (ProcessM, out, MonadIO, liftIO, debug)
+import           Lycopene.Environment (createDatabase)
 import           Lycopene.Core
 
 
 configure :: (MonadIO m) => FilePath -> ProcessM m
 configure target = do
-  let createParentIfMissing = createDirectoryIfMissing . dropFileName
-  createParentIfMissing target
-  hd <- getHomeDirectory
-  hd </> ".lyco"
- 
+  let createParentIfMissing = (createDirectoryIfMissing True) . dropFileName
+      expandHome :: FilePath -> FilePath -> FilePath
+      expandHome home ('~':xs) = home ++ xs
+      expandHome home path     = path
+      tgtIO = (flip expandHome target) `fmap` getHomeDirectory
+  tgt <- liftIO tgtIO
+  (liftIO . createParentIfMissing) tgt
+  exists <- (liftIO . doesFileExist) tgt
+  case exists of
+    True  -> debug $ T.pack ("already exists: " ++ tgt)
+    False -> out $ T.pack ("database created: " ++ tgt)
