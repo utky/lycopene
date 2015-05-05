c8533feae22ca6b2946b6593ed02318db221355e
utky <ilyaletre@gmail.com>
2015-05-05 16:50:05 +0900

    removed errornous block

--- main/Main.hs
+++ main/Main.hs
@@ -3,7 +3,7 @@ module Main
         ) where
 
 import           Lycopene.Option (parseLycoCommand)
-import           Lycopene.Process (runProcess', processCommand)
+import qualified Lycopene.Process as P
 import           Lycopene.Resource (configResource)
 import           Lycopene.Core (runLycopene)
 import           System.Exit 
@@ -11,15 +11,13 @@ import           System.Exit
 
 main :: IO ()
 main = do
-  let runCommand = runProcess' . processCommand
+  let runCommand a b = P.runProcess' $ P.processCommand a b
   cmd <- parseLycoCommand
   cfg <- configResource cmd
-  case eitherConfig of
-    Right cfg -> runCommand cfg cmd >>= exitWith . mapExit
-    Left e    -> putStrLn "resource error" >> exitFailure
+  runCommand cfg cmd >>= exitWith . mapExit
 
 -- | TODO: vary exit code
-mapExit :: Ps.Result -> ExitCode
-mapExit Ps.Success = ExitSuccess
-mapExit _          = ExitFailure 1
+mapExit :: P.Result -> ExitCode
+mapExit P.Success = ExitSuccess
+mapExit _         = ExitFailure 1
 
