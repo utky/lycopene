f77c435962706046b2b40aad25fe53052a7d491c
utky <ilyaletre@gmail.com>
2015-05-14 08:11:02 +0900

    clean up redundant import

--- src/Lycopene/Process/Configure.hs
+++ src/Lycopene/Process/Configure.hs
@@ -1,10 +1,9 @@
 module Lycopene.Process.Configure where
 
 import qualified Data.Text as T
-import           Control.Monad.Trans (lift)
-import           System.FilePath ((</>), dropFileName)
-import           System.Directory (createDirectoryIfMissing, getHomeDirectory, doesFileExist)
-import           Lycopene.Process.Core (ProcessR, Result, out, MonadIO, liftIO, debug, failure, runDomain)
+import           System.FilePath (dropFileName)
+import           System.Directory (createDirectoryIfMissing, doesFileExist)
+import           Lycopene.Process.Core (ProcessR, out, MonadIO, liftIO, debug, failure, runDomain)
 import           Lycopene.Environment (createDatabase)
 import           Lycopene.Core
 import           Lycopene.Core.Project as Project
@@ -14,12 +13,17 @@ import           Lycopene.Core.Sprint as Sprint
 configure :: (MonadIO m) => FilePath -> ProcessR m
 configure target = do
   let createParentIfMissing = (createDirectoryIfMissing True) . dropFileName
-      dbOperation = createDatabase >> Project.inbox >> Sprint.inboxDefault >> Sprint.inboxBacklog >> return ()
-
   -- FIXME: debug print
   -- debug $ T.pack target
   exists <- liftIO $ createParentIfMissing target >> doesFileExist target
   case exists of
     True  -> (debug $ T.pack ("already exists: " ++ target)) >> failure
-    False -> runDomain dbOperation >> (out $ T.pack ("database created: " ++ target))
+    False -> runDomain setupSchema >> (out $ T.pack target)
 
+setupSchema :: LycopeneT Persist ()
+setupSchema = do
+  createDatabase
+  _ <- Project.inbox
+  _ <- Sprint.inboxDefault
+  _ <- Sprint.inboxBacklog
+  return ()
